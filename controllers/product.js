d8be2492c8ccecc231764a0809701b309561a4a0
lagerhausenandrew <lagerhausen.3@gmail.com>
2020-11-17 09:44:49 -0600

    removing useless code

--- controllers/product.js
+++ controllers/product.js
@@ -12,21 +12,6 @@ const getAll = (req, res) => {
     })
 }
 
-const getProductById = (req, res) => {
-    Product.findByPk(req.params.productId)
-    .then(foundProduct => {
-        if(foundProducts === null){
-            res.status(constants.BAD_REQUEST).send('ERROR: Incorrect Post Id')
-        }else{
-            res.status(constants.SUCCESS).json(foundProduct)
-        }
-    })
-    .catch(err => {
-        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
-    })
-}
-
 module.exports = {
     getAll,
-    getProductById
 }
--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.get('/all', ctrl.product.getAll);
-router.get('/:product', ctrl.product.getProductById);
 
 module.exports = router;
